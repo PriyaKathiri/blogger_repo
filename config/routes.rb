f211091bf6edeafda7aad60715fcf54f6f482ad6
priya <pkathiri@bna.com>
2014-06-26 09:48:39 -0400

    Finished blog comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Blogger::Application.routes.draw do
-  root to: 'articles#index'
-	
-  resources :articles
+  root to: 'articles#index'	
+  resources :articles do
+  	  resources :comments
+  end
 end
